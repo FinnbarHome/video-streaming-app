e258b6f14fb906a1b574c8cb9f6ad9a71761c49e
FinnbarHome <finnbar.home2@gmail.com>
2025-01-20 22:38:01 +0000

    Video host using env

--- video-host/server.js
+++ video-host/server.js
@@ -2,7 +2,10 @@ const express = require("express");
 const path = require("path");
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT;
+
+console.log(`Connecting to MongoDB: ${mongoUri}`);
+console.log(`Using Video Host Port: ${videoHostPort}`);
 
 // Serve static files from the "videos" directory
 app.use("/videos", express.static(path.join(__dirname, "videos")));
